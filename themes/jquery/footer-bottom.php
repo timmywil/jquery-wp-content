5b213f51747e9fcceb536c37c96e4430c09a65b7
Timo Tijhof <krinkle@fastmail.com>
2026-06-17 14:38:42 +0100

    All: Add referal code to DigitalOcean sponsorship link (#491)
    Requested by DigitalOcean to help measure impact and attribute
    sign-ups to the sponsorshop link.
    
    Note that our account is sponsored, so while I don't know if this
    will add credits to our account or not, it doesn't make a difference
    in that either way DigitalOcean is sponsoring us.

--- themes/jquery/footer-bottom.php
+++ themes/jquery/footer-bottom.php
@@ -27,7 +27,7 @@
 	<p class="copyright">
 		Copyright <?php echo date('Y'); ?> <a href="https://openjsf.org/">OpenJS Foundation</a> and jQuery contributors. All rights reserved. See <a href="https://jquery.com/license/">jQuery License</a> for more information. The <a href="https://openjsf.org/">OpenJS Foundation</a> has registered trademarks and uses trademarks. For a list of trademarks of the <a href="https://openjsf.org/">OpenJS Foundation</a>, please see our <a href="https://trademark-policy.openjsf.org/">Trademark Policy</a> and <a href="https://trademark-list.openjsf.org/">Trademark List</a>. Trademarks and logos not indicated on the <a href="https://trademark-list.openjsf.org/">list of OpenJS Foundation trademarks</a> are trademarks™ or registered® trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them. OpenJS Foundation <a href="https://terms-of-use.openjsf.org/">Terms of Use</a>, <a href="https://privacy-policy.openjsf.org/">Privacy</a>, <a href="https://code-of-conduct.openjsf.org/">Code of Conduct</a>, and <a href="https://www.linuxfoundation.org/cookies">Cookie</a> Policies also apply.
 	</p>
-	<p><a href="https://www.digitalocean.com" class="do-link">Web hosting by Digital Ocean</a> | <a href="https://www.fastly.com/">CDN by Fastly</a> | <a href="https://wordpress.org/" class="wp-link">Powered by WordPress</a></p>
+	<p><a href="https://www.digitalocean.com/?refcode=aa1a2e950a73&amp;utm_campaign=Referral_Invite&amp;utm_medium=Referral_Program&amp;utm_source=badge" class="do-link">Web hosting by Digital Ocean</a> | <a href="https://www.fastly.com/">CDN by Fastly</a> | <a href="https://wordpress.org/" class="wp-link">Powered by WordPress</a></p>
 </div>
 
 <?php
